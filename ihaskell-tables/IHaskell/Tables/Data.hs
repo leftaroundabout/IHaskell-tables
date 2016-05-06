942d9eead037fd999016372c10a8872860409a0d
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2016-05-06 22:24:23 +0200

    Essential column legends.

--- ihaskell-tables/IHaskell/Tables/Data.hs
+++ ihaskell-tables/IHaskell/Tables/Data.hs
@@ -13,7 +13,7 @@
 {-# LANGUAGE FlexibleContexts     #-}
 {-# LANGUAGE UndecidableInstances #-}
 
-module IHaskell.Tables.Data (Table, tabular, TabShow) where
+module IHaskell.Tables.Data (Table, tabular, tableWithLegend, TabShow) where
 
 
 import IHaskell.Display
@@ -22,6 +22,7 @@ import IHaskell.Display.Blaze
 import qualified Text.Blaze.Html4.Strict as H
 import qualified Text.Blaze.Html4.Strict.Attributes as HA
 
+import Data.Monoid
 
 -- | A tabular view of Haskell data.
 data Table d = Table { getTableContent :: d
@@ -36,40 +37,52 @@ tableWithLegend legend x = Table x $ pure legend
 
 type HTML = H.Html
 
-data TableLevel = TabListItem | TabAtomic | TabCols | TabRows
+data TableLevel = TabListItem | TabAtomic
+                | TabTupCols  | TabTupRows
+                | TabListCols | TabListRows
 
-class TabShow s where
+class (Monoid (SharedPrecomputation s)) => TabShow s where
   type TSDLegend s :: *
   type TSDLegend s = String
   type SharedPrecomputation s :: *
   type SharedPrecomputation s = ()
+  
   precompute :: s -> SharedPrecomputation s
+  precompute _ = mempty
+  
   showAsTable :: Maybe (TSDLegend s) -> SharedPrecomputation s -> s
                    -> HTML  -- might become Javascript instead.
-  showListAsTable :: Maybe (TSDLegend s) -> SharedPrecomputation s -> [s]
-                   -> HTML
-  showListAsTable _ pc i = foldMap (rowEnv . showAsTable Nothing pc) i
+  
+  showListAsTable :: (TabShow (TSDLegend s), Monoid (SharedPrecomputation (TSDLegend s)))
+            => Maybe (TSDLegend s) -> SharedPrecomputation s -> [s] -> HTML
+  showListAsTable l pc i = addLegend l $ foldMap (rowEnv . showAsTable Nothing pc) i
    where rowEnv = case tableLevel . Just $ head i of
           TabListItem -> id
           TabAtomic   -> H.td
-          TabCols     -> H.tr
-          TabRows     -> H.td . H.table
+          TabTupCols  -> H.tr
+          TabTupRows  -> H.td . H.table
+          TabListCols -> H.tr
+          TabListRows -> H.td . H.table
+         addLegend Nothing t = t
+         addLegend (Just l) t = H.thead (rowEnv $ showAsTable Nothing mempty l)
+                             <> H.tbody t
   tableLevel :: Functor p => p s -> TableLevel
   tableLevel _ = TabAtomic
 
+instance TabShow () where
+  type TSDLegend () = ()
+  showAsTable _ _ () = mempty
+  showListAsTable _ _ _ = mempty
+  tableLevel _ = TabListItem
 instance TabShow Int where
-  precompute _ = ()
   showAsTable _ _ i = H.toHtml i
 instance TabShow Integer where
-  precompute _ = ()
   showAsTable _ _ i = H.toHtml i
 instance TabShow Char where
-  type TSDLegend Char = ()
-  precompute _ = ()
   showAsTable _ _ i = H.toHtml i
   showListAsTable _ _ i = H.toHtml i
   tableLevel _ = TabListItem
-instance (TabShow s, TabShow (TSDLegend s), Monoid (SharedPrecomputation s))
+instance ( TabShow s, TabShow (TSDLegend s) )
            => TabShow [s] where
   type TSDLegend [s] = TSDLegend s
   type SharedPrecomputation [s] = SharedPrecomputation s
@@ -77,9 +90,32 @@ instance (TabShow s, TabShow (TSDLegend s), Monoid (SharedPrecomputation s))
   showAsTable = showListAsTable
   tableLevel pq = case tableLevel $ fmap head pq of
           TabListItem -> TabAtomic
-          TabAtomic   -> TabCols
-          TabCols     -> TabRows
-          TabRows     -> TabCols
+          TabAtomic   -> TabListCols
+          TabTupCols  -> TabListRows
+          TabTupRows  -> TabListCols
+          TabListCols -> TabListRows
+          TabListRows -> TabListCols
+  
+instance (TabShow s, TabShow t) => TabShow (s,t) where
+  type TSDLegend (s,t) = (TSDLegend s, TSDLegend t)
+  type SharedPrecomputation (s,t) = (SharedPrecomputation s, SharedPrecomputation t)
+  precompute (x,y) = (precompute x, precompute y)
+  showAsTable l (px,py) (x,y) = colEnv (showAsTable (fmap fst l) px x)
+                             <> colEnv (showAsTable (fmap snd l) py y)
+   where colEnv = case tableLevel $ Just x of
+          TabListItem -> H.td
+          TabAtomic   -> H.td
+          TabTupCols  -> id
+          TabTupRows  -> id
+          TabListCols -> H.tr
+          TabListRows -> H.td . H.table
+  tableLevel pq = case tableLevel $ fmap fst pq of
+          TabListItem -> TabTupCols
+          TabAtomic   -> TabTupCols
+          TabTupCols  -> TabTupCols
+          TabTupRows  -> TabTupRows
+          TabListCols -> TabTupRows
+          TabListRows -> TabTupCols
 
 instance (Show s, TabShow s) => IHaskellDisplay (Table s) where
   display (Table i l)
