deec94ed1b60e67d6b6564d0246bc020c2eaf6b6
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2016-05-06 16:20:30 +0200

    Initial commit

---
+++ ihaskell-tables/IHaskell/Tables/Data.hs
@@ -0,0 +1,16 @@
+-- |
+-- Module      : IHaskell.Tables.Data
+-- Copyright   : (c) Justus Sagemüller 2016
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) sagemueller $ geo.uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+
+module IHaskell.Tables.Data where
+
+
+import IHaskell.Display
+
