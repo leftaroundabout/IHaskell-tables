d3ed24113c78f74454fc52fe1a13d2a1bd9c8e92
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2016-05-06 17:32:33 +0200

    Necessary modules to actuall get the class working in Jupyter.

---
+++ ihaskell-tables/IHaskell/Display/Tables.hs
@@ -0,0 +1,18 @@
+-- |
+-- Module      : IHaskell.Display.Tables
+-- Copyright   : (c) Justus Sagemüller 2016
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) sagemueller $ geo.uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+
+{-# LANGUAGE TypeFamilies      #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module IHaskell.Display.Tables () where
+
+
+import IHaskell.Tables.Data
+
